0a95240b8c055993f0f9ac6bcf5e5dbb9f9e4f62
UNOWN7 <115024085+UNOWN7@users.noreply.github.com>
2023-12-22 17:58:42 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { INTEGER } = require('sequelize');
+const app = express();
+const PORT = 3000;
+app.use(express.json())
+app.get('/getName',(req,res)=>{
+        let { numb1 , numb2 , operator } = req.body;
+        const num1 = parseFloat(numb1);
+        const num2 = parseFloat(numb2);
+
+        let result;
+        if(isNaN(num1) || isNaN(num2) ){
+            return res.status(400).send('Invalid Input');
+        }else{
+                if(operator==='add'){
+                    result=num1 + num2;
+                    
+                }else if(operator==='sub'){
+                    result=num1-num2;
+                    
+                }else if(operator==="multiply"){
+                    result=num1*num2;
+                    
+                }else if(operator==="divide"){
+                    if(num2==0){
+                    res.send("Number can't divide by zero")
+                    }else{
+                        result=num1/num2;
+                        
+                    }
+                }
+            res.json("Result : " + result );
+    };
+    
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
